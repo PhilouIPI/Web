68afd32d5e3e47bceded0a6b4dcb1cca261dc49a
PhilouIPI <philippe.naovarath@campus-igs-toulouse.fr>
2020-07-09 14:01:55 +0200

    ajout Reporter dans Bean

---
+++ src/main/java/Bean/Reporter.java
@@ -0,0 +1,32 @@
+package Bean;
+
+public class Reporter {
+    private int id ;
+    private String pseudo;
+    private int credit;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPseudo() {
+        return pseudo;
+    }
+
+    public void setPseudo(String pseudo) {
+        this.pseudo = pseudo;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+}
+
