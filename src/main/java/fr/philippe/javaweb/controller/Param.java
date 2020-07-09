41fd322e4fdd158b612367a66a21e5fb9b2fa38b
PhilouIPI <philippe.naovarath@campus-igs-toulouse.fr>
2020-07-09 08:56:12 +0200

    nom en parametre dans url http://localhost:8080/java_web/simple?pseudo=Philou

---
+++ src/main/java/fr/philippe/javaweb/controller/Param.java
@@ -0,0 +1,19 @@
+package fr.philippe.javaweb.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Param extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        final String MESSAGE_PARAM = req.getParameter("pseudo");
+        out.println("Bonjour "+ MESSAGE_PARAM + " ! </br>");
+            }
+}
